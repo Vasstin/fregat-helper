029d708215c5a8b57f2258c4a214b83b1011abb6
Vasstin <vastin115408@gmail.com>
2025-06-04 14:53:13 +0300

    change pppoe config

--- src/OnuConfig/Config.js
+++ src/OnuConfig/Config.js
@@ -3,14 +3,19 @@ import React from "react";
 
 const Config = (props) => {
   //let vlan = props.configData.interface.split("/")[2].split(":")[0]
-  console.log(props.configType);
+  console.log(props);
   let content;
   switch (props.configType) {
     case "Switch":
       content = (
         <Box>
           <p>conf term</p>
-          <p>interface {props.configData.interface}</p>
+          <p>interface gpon-olt_1/{props.configData.plate}/{props.configData.ports[props.index]}</p>
+          <p>
+            onu {props.configData.Onuid} type zte601 sn {props.configData.onuSn}
+          </p>
+          <p>exit</p>
+          <p>interface gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           <p>description {props.configData.desc}</p>
           <p>tcont 1 name fregat profile fregat</p>
           <p>gemport 1 name fregat tcont 1</p>
@@ -20,7 +25,7 @@ const Config = (props) => {
             service-port 2 vport 1 user-vlan {props.configData.vlan} transparent
           </p>
           <p>exit</p>
-          <p>pon-onu-mng {props.configData.interface}</p>
+          <p>pon-onu-mng gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           <p>service MANAGE gemport 1 vlan 77</p>
           <p>
             service PPPoE-{props.configData.vlan} gemport 1 vlan{" "}
@@ -34,7 +39,12 @@ const Config = (props) => {
     case "PPPoE":
       content = (
         <Box>
-          <p>interface {props.configData.interface}</p>
+           <p>interface gpon-olt_1/{props.configData.plate}/{props.configData.ports[props.index]}</p>
+          <p>
+            onu {props.configData.Onuid} type zte601 sn {props.configData.onuSn}
+          </p>
+          <p>exit</p>
+          <p>interface gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           <p>description {props.configData.desc}</p>
           <p>tcont 1 name fregat profile fregat</p>
           <p>gemport 1 name fregat tcont 1</p>
@@ -43,22 +53,24 @@ const Config = (props) => {
             {props.configData.vlan}
           </p>
           <p>security max-mac-learn 2 vport 1</p>
-          <p>pon-onu-mng {props.configData.interface}</p>
+          <p>exit</p>
+          <p>pon-onu-mng gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           <p>service internet gemport 1 cos 0 vlan {props.configData.vlan}</p>
           <p>vlan port eth_0/1 mode tag vlan {props.configData.vlan}</p>{" "}
+          <p>exit</p>
         </Box>
       );
       break;
     case "ZTE_PPPoE":
       content = (
         <Box>
-          <p>config terminal</p>
-          <p>interface gpon-olt_{props.configData.oltInterface}</p>
+          {/* <p>config terminal</p> */}
+          <p>interface gpon-olt_1/{props.configData.plate}/{props.configData.ports[props.index]}</p>
           <p>
-            onu {props.configData.id} type zte601 sn {props.configData.onu}
+            onu {props.configData.Onuid} type zte601 sn {props.configData.onuSn}
           </p>
           <p>exit</p>
-          <p>interface {props.configData.interface}</p>
+          <p>interface gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           {props.configData.desc ? (
             <p>description {props.configData.desc}</p>
           ) : null}
@@ -70,7 +82,7 @@ const Config = (props) => {
           </p>
           <p>security max-mac-learn 2 vport 1</p>
           <p>exit</p>
-          <p>pon-onu-mng {props.configData.interface}</p>
+          <p>pon-onu-mng gpon-onu_1/{props.configData.plate}/{props.configData.ports[props.index]}:{props.configData.Onuid}</p>
           <p>service internet gemport 1 cos 0 vlan {props.configData.vlan}</p>
           <p>vlan port eth_0/1 mode tag vlan {props.configData.vlan}</p>
           <p>exit</p>
--- src/OnuConfig/OnuConfig.js
+++ src/OnuConfig/OnuConfig.js
@@ -13,50 +13,85 @@ const OnuConfig = (props) => {
 
   const handleConfigData = (event) => {
     const { name, value } = event.target;
-
-    if (name === "interface") {
-      let obj = {
-        oltInterface: "",
-        id: "",
-        vlan: "",
-      };
-      const underscoreSplit = value.split("_");
-      const slashSplit = value.split("/");
-
-      if (
-        underscoreSplit.length > 1 &&
-        underscoreSplit[1].includes(":") &&
-        slashSplit.length > 2 &&
-        slashSplit[2].includes(":")
-      ) {
-        try {
-          obj.oltInterface = underscoreSplit[1].split(":")[0];
-          obj.id = underscoreSplit[1].split(":")[1];
-          const data = slashSplit[2].split(":")[0];
-          obj.vlan = data < 10 ? "80" + data : "8" + data;
-        } catch (e) {
-          obj.oltInterface = "";
-          obj.id = "";
-          obj.vlan = "";
+    if (name === "ports") {
+      let arr = value.split(" ");
+      let newArr = [];
+      arr.forEach((item) => {
+        if (item.includes("-")) {
+          let innerItem = item.split("-").map(Number);
+          for (let i = innerItem[0]; i <= innerItem[1]; i++) {
+            newArr.push(i);
+          }
+        } else {
+          newArr.push(Number(item));
         }
-      }
-      for (const prop in obj) {
-        setConfigData((prev) => ({
-          ...prev,
-          [prop]: obj[prop],
-        }));
-      }
+      });
+      arr[0] === ""
+        ? setConfigData((prev) => ({
+            ...prev,
+            [name]: [1],
+          }))
+        : setConfigData((prev) => ({
+            ...prev,
+            [name]: newArr,
+          }));
+    } else {
+      setConfigData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
     }
+    // if (name === "plate") {
+    //   console.log(name, value);
+    //   let obj = {
+    //     oltInterface: "",
+    //     id: "",
+    //     vlan: "",
+    //   };
+    //   const underscoreSplit = value.split("_");
+    //   const slashSplit = value.split("/");
 
-    setConfigData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    //   if (
+    //     underscoreSplit.length > 1 &&
+    //     underscoreSplit[1].includes(":") &&
+    //     slashSplit.length > 2 &&
+    //     slashSplit[2].includes(":")
+    //   ) {
+    //     try {
+    //       obj.oltInterface = underscoreSplit[1].split(":")[0];
+    //       obj.id = underscoreSplit[1].split(":")[1];
+    //       const data = slashSplit[2].split(":")[0];
+    //       obj.vlan = data < 10 ? "80" + data : "8" + data;
+    //     } catch (e) {
+    //       obj.oltInterface = "";
+    //       obj.id = "";
+    //       obj.vlan = "";
+    //     }
+    //   }
+    //   for (const prop in obj) {
+    //     setConfigData((prev) => ({
+    //       ...prev,
+    //       [prop]: obj[prop],
+    //     }));
+    //   }
+    // }
+    // else if (name === "ports") {
+    //   setConfigData((prev) => ({
+    //     ...prev,
+    //     [name]: 555,
+    //   }));
+    // } else {
+    // }
+    // setConfigData((prev) => ({
+    //   ...prev,
+    //   [name]: value,
+    // }));
   };
+
   let handleType = (event) => {
     setConfigType(event.target.value);
   };
-
+  console.log(configData);
   return (
     <Box>
       <Typography
@@ -132,9 +167,9 @@ const OnuConfig = (props) => {
               width: "250px",
             }}
             color="primary"
-            name="interface"
+            name="plate"
             id="outlined-basic"
-            label="Interface"
+            label="Plate"
             variant="outlined"
             size="small"
             onChange={handleConfigData}
@@ -145,9 +180,35 @@ const OnuConfig = (props) => {
               width: "250px",
             }}
             color="primary"
-            name="onu"
+            name="onuSn"
             id="outlined-basic"
-            label="Onu"
+            label="OnuSn"
+            variant="outlined"
+            size="small"
+            onChange={handleConfigData}
+          ></TextField>
+          <TextField
+            sx={{
+              marginBottom: "10px",
+              width: "250px",
+            }}
+            color="primary"
+            name="ports"
+            id="outlined-basic"
+            label="Ports"
+            variant="outlined"
+            size="small"
+            onChange={handleConfigData}
+          ></TextField>
+          <TextField
+            sx={{
+              marginBottom: "10px",
+              width: "250px",
+            }}
+            color="primary"
+            name="Onuid"
+            id="outlined-basic"
+            label="OnuId"
             variant="outlined"
             size="small"
             onChange={handleConfigData}
@@ -158,7 +219,17 @@ const OnuConfig = (props) => {
             width: "50%",
           }}
         >
-          <Config configData={configData} configType={configType} />
+          {configData.ports === undefined ? (
+            <div></div>
+          ) : (
+            configData.ports.map((item, index) => (
+              <Config
+                configData={configData}
+                index={index}
+                configType={configType}
+              />
+            ))
+          )}
         </Box>
       </Box>
     </Box>
